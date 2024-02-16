5e2429c6c30282ad016ea33d295054420252c3e2
LinkangZhan <linkangzhan@foxmail.com>
2024-02-16 15:59:04 -0500

    docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone',
+  };
+  
+  export default nextConfig;
+  
